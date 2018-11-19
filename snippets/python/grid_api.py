f968e4000be50d921a130108cc2906dc9c2c76fe
Tim <timothyolaleke@gmail.com>
2018-11-19 12:10:14 +0100

    Changed function name

--- snippets/python/grid_api.py
+++ snippets/python/grid_api.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://api.grid-nigeria.org/"
 
-def fetch(resource_name, size=200, page=1, sort_by="global_id", fields=""):
+def datasets(resource_name, size=200, page=1, sort_by="global_id", fields=""):
     endpoint = "{}{}".format(url, resource_name)
     querystring = {"size":size,"page":page,"sort_by":sort_by,"fields":fields}
     response = requests.request("GET", endpoint, params=querystring)
