6612051da713065e9675ef9bca5d0be87f9b952a
Tim <timothyolaleke@gmail.com>
2018-11-27 14:31:01 +0100

    Fixed Python Code

--- snippets/python/grid_api.py
+++ snippets/python/grid_api.py
@@ -6,4 +6,4 @@ def getDatasets(resource_name, size=200, page=1, sort_by="global_id", fields="",
     endpoint = "{}{}".format(url, resource_name)
     querystring = {"size":size,"page":page,"sort_by":sort_by,"fields":fields, "cql":cql }
     response = requests.request("GET", endpoint, params=querystring)
-    return response.url
+    return response.text
